b7f8687e18d9a6bd36963679bc7c0eb4137031a8
RobertIlea <robert.ilea03@e-uvt.ro>
2023-12-21 23:20:59 +0200

    Test to see if ADD/DELETE works

--- ATM/MainATM.java
+++ ATM/MainATM.java
@@ -2,6 +2,14 @@ package ATM;
 
 public class MainATM {
     public static void main(String [] args){
+        User user = new User("Marinel", "Dorel", "RO12312442122222", 1234,132132);
+        //User.addUser(user);
+        User getUser = User.getUserDataByIban("RO123456789123456789");
+        if(getUser != null){
+            User.removeUser(getUser);
+        } else {
+            System.out.println("User not found");
+        }
 
     }
 }
